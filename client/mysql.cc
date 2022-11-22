bbc06b3ae2c481971d4fbe35f65beb0555a34728
Anel Husakovic <anel@mariadb.org>
2022-11-22 18:05:03 +0100

    Remove spaces at beginning of line

--- client/mysql.cc
+++ client/mysql.cc
@@ -5776,6 +5776,9 @@ static int deinit_alias()
 static int com_alias(String *buffer __attribute__((unused)),
                      char *line)
 {
+  while (my_isspace(charset_info,*line))
+    line++;
+
   char *ptr= strchr(line,' ');
   bool unused;
 
