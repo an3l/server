cff7513977c2144553b03a41489bf8a2bf02c672
Anel Husakovic <anel@mariadb.org>
2023-01-19 14:51:17 +0100

    MDEV-21322: show replica hosts update result with GTID states
    + We are trying to extend the command `SHOW REPLICA HOSTS` that is
    executed on master, with columns `GTID_state_sent` and `GTID_state_ack`.
    In order to achieve that we have to extend `thd->slave_info` struct with
    2 new entries. This struct needs to be populated and  must be accessable to primary.
    
    + The first column `GTID_state_sent`:
      - It is populated by the last event that primary has sent to replica
        (only for semi-sync slave connection, but without need to know
        that replica actually obtained request,
        like it is case in asynchronous replication).
      - Primary executes `mysql_binlog` that first starts the `binlog_dump` thread.
        That thread starts `ack_receiver` thread, that returns second column
        of interest (in first iteration we don't care about).
        After `binlog_dump` thread creating, file is sent with
        `send_one_binlog_file()` function.
        This function is sending event with `send_events()` function,
        during which `send_event_to_slave()` function is executed.
        On that place we are creating the `thd->slave_info->gtid_state_sent` struct.
    
    + The second column `GTID_state_ack`:
      - It is populated by the `reply_packet_binlog` (a.k.a. `ack`) that is called,
        only for semi-sync replication. It is called on 2 places:
        a) during `binlog_dump` creation, after thread is added to
        `ack_reciver` and after thread is added as semi-sync slave. This
        should be first ack received from replica to primary.
        b) constantly in running phase of `ack_receiver` thread `run()`.
      - A the end of function `reply_packet_binlog` we are creating the
      struct `GTID_state_ack`
    
    Reviewer:<brandon.nesterenko@mariadb.com>

--- sql/semisync_master.h
+++ sql/semisync_master.h
@@ -28,6 +28,32 @@ extern PSI_mutex_key key_LOCK_binlog;
 extern PSI_cond_key key_COND_binlog_send;
 #endif
 
+
+/*
+  structure to save transaction log filename and position
+*/
+typedef struct Trans_binlog_info {
+  my_off_t log_pos;
+  char log_file[FN_REFLEN];
+} Trans_binlog_info;
+
+
+struct Slave_info
+{
+  uint32 server_id;
+  uint32 master_id;
+  char host[HOSTNAME_LENGTH*SYSTEM_CHARSET_MBMAXLEN+1];
+  char user[USERNAME_LENGTH+1];
+  char password[MAX_PASSWORD_LENGTH*SYSTEM_CHARSET_MBMAXLEN+1];
+  uint16 port;
+/* Following attributes are slave info, since they are used for show_slave_hosts
+   that is executed on master, when called meaning will be oposite on master side
+*/
+  Trans_binlog_info gtid_state_sent;
+  Trans_binlog_info gtid_state_ack;
+};
+
+
 struct Tranx_node {
   char              log_name[FN_REFLEN];
   my_off_t          log_pos;
@@ -516,7 +542,7 @@ class Repl_semi_sync_master
   void remove_slave();
 
   /* It parses a reply packet and call report_reply_binlog to handle it. */
-  int report_reply_packet(uint32 server_id, const uchar *packet,
+  int report_reply_packet(THD *thd, uint32 server_id, const uchar *packet,
                         ulong packet_len);
 
   /* In semi-sync replication, reports up to which binlog position we have
@@ -531,7 +557,7 @@ class Repl_semi_sync_master
    * Return:
    *  0: success;  non-zero: error
    */
-  int report_reply_binlog(uint32 server_id,
+  int report_reply_binlog(THD *thd, uint32 server_id,
                           const char* log_file_name,
                           my_off_t end_offset);
 
