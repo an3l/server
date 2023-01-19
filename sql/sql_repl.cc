4ee324508c70696ce86ff1fa8ca3613d6c1f656f
Anel Husakovic <anel@mariadb.org>
2023-01-19 14:51:17 +0100

    MDEV-21322: show replica hosts update result with GTID states

--- sql/sql_repl.cc
+++ sql/sql_repl.cc
@@ -2043,6 +2043,15 @@ send_event_to_slave(binlog_send_info *info, Log_event_type event_type,
     return "run 'before_send_event' hook failed";
   }
 
+  /*
+    We want to store data slave_info on primary that will be used for
+    show_slave_hosts_callback. We are interested in GTID sent by primary
+    to replica.
+  */
+  strncpy(info->thd->slave_info->gtid_state_sent.log_file,
+            info->log_file_name + info->dirlen, strlen(info->log_file_name)-info->dirlen);
+  info->thd->slave_info->gtid_state_sent.log_pos= pos;
+
   if (my_net_write(info->net, (uchar*) packet->ptr(), len))
   {
     info->error= ER_UNKNOWN_ERROR;
@@ -2748,7 +2757,6 @@ static int send_events(binlog_send_info *info, IO_CACHE* log, LOG_INFO* linfo,
         ((info->errmsg= send_event_to_slave(info, event_type, log,
                                            ev_offset, &info->error_gtid))))
       return 1;
-
     if (unlikely(info->send_fake_gtid_list) &&
         info->gtid_skip_group == GTID_SKIP_NOT)
     {
