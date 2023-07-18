31568fb441f1281b24efbe1e62efaa1c051b1748
Daniel Black <daniel@mariadb.org>
2023-07-18 13:37:30 +1000

    MDEV-4827 mysqldump --dump-slave=2 --master-data=2 doesn't record both
    Recording both is useful on a replication relay.
    
    If a =2, commented is selected, allow the alternate option to exist.
    
    This still disables --dump-slave=1 --master-data=1 as having the
    a CHANGE MASTER TO and START SLAVE on different positions would be
    confusing and dangerious to the data.
    
    Based of code from Elena Stepanova.

--- client/mysqldump.c
+++ client/mysqldump.c
@@ -1226,8 +1226,9 @@ static int get_options(int *argc, char ***argv)
   if (opt_slave_data)
   {
     opt_lock_all_tables= !opt_single_transaction;
-    opt_master_data= 0;
     opt_delete_master_logs= 0;
+    if (opt_slave_data != MYSQL_OPT_SLAVE_DATA_COMMENTED_SQL)
+      opt_master_data= 0;
   }
 
   /* Ensure consistency of the set of binlog & locking options */
@@ -1242,7 +1243,8 @@ static int get_options(int *argc, char ***argv)
   if (opt_master_data)
   {
     opt_lock_all_tables= !opt_single_transaction;
-    opt_slave_data= 0;
+    if (opt_master_data != MYSQL_OPT_MASTER_DATA_COMMENTED_SQL)
+      opt_slave_data= 0;
   }
   if (opt_single_transaction || opt_lock_all_tables)
     lock_tables= 0;
