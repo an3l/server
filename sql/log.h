57dbdcd636aee7713151ddd8bce1a08218161725
Anel Husakovic <anel@mariadb.org>
2023-12-07 12:42:24 +0100

    Pure virtual - linker problem

--- sql/log.h
+++ sql/log.h
@@ -597,6 +597,7 @@ class binlog_cache_data;
 struct rpl_gtid;
 struct wait_for_commit;
 
+
 class MYSQL_BIN_LOG: public TC_LOG, private Event_log
 {
   /** The instrumentation key to use for @ LOCK_index. */
@@ -1010,7 +1011,7 @@ public:
   void mark_xid_done(ulong cookie, bool write_checkpoint);
   void make_log_name(char* buf, const char* log_ident);
   bool is_active(const char* log_file_name);
-  virtual bool can_purge_log(const char *log_file_name);
+  virtual bool can_purge_log(const char *log_file_name)=0;
   int update_log_index(LOG_INFO* linfo, bool need_update_threads);
   int rotate(bool force_rotate, bool* check_purge);
   void checkpoint_and_purge(ulong binlog_id);
@@ -1164,6 +1165,9 @@ public:
   */
   my_off_t binlog_end_pos;
   char binlog_end_pos_file[FN_REFLEN];
+  virtual ~MYSQL_BIN_LOG(){}
+  friend class MYSQL_BINARY_LOG;
+  friend class MYSQL_RELAY_LOG;
 };
 
 
@@ -1175,11 +1179,7 @@ class MYSQL_BINARY_LOG: public MYSQL_BIN_LOG
     {
       is_relay_log= false;
     }
-   bool can_purge_log(const char *log_file_name) override
-   {
-      return 1;
-      // return MYSQL_BIN_LOG::can_purge_log(log_file_name);
-   }
+   bool can_purge_log(const char *log_file_name) override;
 };
 
 
@@ -1191,11 +1191,7 @@ class MYSQL_RELAY_LOG: public MYSQL_BIN_LOG
     {
       is_relay_log= true;
     }
-   bool can_purge_log(const char *log_file_name) override
-   {
-      return 0;
-      // return MYSQL_BIN_LOG::can_purge_log(log_file_name);
-   }
+   bool can_purge_log(const char *log_file_name) override;
 };
 
 
