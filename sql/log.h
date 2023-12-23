88b7f7d398245d9c3daece2df69cafab1822dcfc
Anel Husakovic <anel@mariadb.org>
2023-12-23 10:50:15 +0100

    Compile problem

--- sql/log.h
+++ sql/log.h
@@ -1010,7 +1010,9 @@ public:
   void mark_xid_done(ulong cookie, bool write_checkpoint);
   void make_log_name(char* buf, const char* log_ident);
   bool is_active(const char* log_file_name);
+#ifdef HAVE_REPLICATION
   virtual bool can_purge_log(const char *log_file_name) = 0;
+#endif
   int update_log_index(LOG_INFO* linfo, bool need_update_threads);
   int rotate(bool force_rotate, bool* check_purge);
   void checkpoint_and_purge(ulong binlog_id);
@@ -1173,7 +1175,9 @@ class MYSQL_BINARY_LOG: public MYSQL_BIN_LOG
   public:
   MYSQL_BINARY_LOG(uint *sync_period, bool is_relay_log= 0)
     :MYSQL_BIN_LOG(sync_period, is_relay_log) {}
+#ifdef HAVE_REPLICATION
   bool can_purge_log(const char *log_file_name) override;
+#endif
 };
 
 
@@ -1182,7 +1186,9 @@ class MYSQL_RELAY_LOG: public MYSQL_BIN_LOG
   public:
   MYSQL_RELAY_LOG(uint *sync_period, bool is_relay_log= 1)
     :MYSQL_BIN_LOG(sync_period, is_relay_log) {}
+#ifdef HAVE_REPLICATION
   bool can_purge_log(const char *log_file_name) override;
+#endif
 };
 
 
